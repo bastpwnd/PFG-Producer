e2b6ca875b49734220618d527ceb0a4f3b7a981f
Ivan Martin Alonso <ivan.martin.alonso.89@gmail.com>
2020-03-26 16:36:24 +0100

    First commit ,Empty project

---
+++ src/main/java/com/example/demo/ProducerApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.MessageFormat;
+import java.util.Calendar;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+    private static final Log log = LogFactory.getLog(ProducerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+
+        long startTime = Calendar.getInstance().getTimeInMillis();
+
+        long endTime = Calendar.getInstance().getTimeInMillis();
+        double startingAppTimeInSeconds = (endTime - startTime) / 1000.0;
+
+        log.info("--------------------------------------------------------------------");
+        log.info(
+                MessageFormat.format(
+                        "|||| Producer - Ivan Martin - PFG -  Application started successfully in {0} seconds ||||",
+                        startingAppTimeInSeconds));
+        log.info("--------------------------------------------------------------------");
+
+
+
+
+    }
+
+}
